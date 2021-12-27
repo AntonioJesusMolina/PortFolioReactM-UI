3bfc99d355089579941f8ff8689ae3984d3c3748
Antonio Jesus Molina Espinosa <amolesp363@g.educaand.es>
2021-12-27 11:52:11 +0100

    Añadimos el proyecto portfolio

--- src/components/mockData.js
+++ src/components/mockData.js
@@ -1,6 +1,7 @@
-import React from "react";
+
 import GifJavascript from "../images/buscagifjava.PNG";
 import GifReact from "../images/buscagifsreact.PNG";
+import PortFolio from "../images/portfolio.PNG";
 
 const mockData = [
   {
@@ -12,6 +13,11 @@ const mockData = [
     title: "Buscador de Gif con React",
     image: GifReact,
     link: "https://github.com/AntonioJesusMolina/Proyecto-Buscador-gifs-javascript-html",
+  },
+  {
+    title: "PortFolio React con MUI",
+    image: PortFolio,
+    link: "https://github.com/AntonioJesusMolina/PortFolioReactM-UI.git",
   },
 ];
 
